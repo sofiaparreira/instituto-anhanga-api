d21a58fe921a03dee210a1566921ea0dd9c2a54d
sofiaparreira <sofiapparreira@gmail.com>
2025-08-23 11:36:28 -0300

    feat: get by id do animal

--- src/controllers/petController.ts
+++ src/controllers/petController.ts
@@ -8,6 +8,22 @@ export async function createPet(req: Request, res: Response) {
         const pet = await PetModel.create(body)
         return res.status(201).json(pet)
     } catch (e: any) {
-        Logger.error(`Erro no sistema: ${e.message}`)
+        Logger.error(`Erro no sistema ao criar pet: ${e.message}`)
     }
 }
+
+export async function getPetById (req:Request, res:Response) {
+    try {
+        const id = req.params.id;
+        const pet = await PetModel.findById(id);
+
+        if(!pet){
+            return res.status(404).json({error: "Pet não encontrado"})
+        }
+        return res.status(200).json(pet)
+    } catch (e:any) {
+         Logger.error(`Erro no sistema ao get by id pet: ${e.message}`)
+    }
+}
+
+
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,5 +1,5 @@
 import {Router, Request, Response} from "express";
-import { createPet } from "../controllers/petController";
+import { createPet, getPetById } from "../controllers/petController";
 import { validate } from "../middlewares/handleValidation";
 import { petBodyValidation } from "../middlewares/petMiddleware";
 const router = Router();
@@ -10,4 +10,5 @@ export default router
     .get("/test", (req: Request, res: Response) => {
     res.status(200).send("API funcionando")
     })
-    .post("/pet", petBodyValidation(), validate, createPet);
+    .post("/pet", petBodyValidation(), validate, createPet)
+    .get("/pet/:id", getPetById);
